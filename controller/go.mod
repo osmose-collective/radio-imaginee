9d57dbbf42f9860670949cf80feb45dcc0fa11e6
Manfred Touron <m@42.am>
2019-04-22 02:28:47 +0200

    feat: initial version of the web interface + use a dedicated API router

--- controller/go.mod
+++ controller/go.mod
@@ -4,7 +4,9 @@ go 1.12
 
 require (
 	github.com/go-chi/chi v4.0.2+incompatible
+	github.com/go-chi/render v1.0.1
 	github.com/reiver/go-oi v0.0.0-20160325061615-431c83978379 // indirect
 	github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e
+	golang.org/x/net v0.0.0-20190419010253-1f3472d942ba // indirect
 	gopkg.in/urfave/cli.v2 v2.0.0-20180128182452-d3ae77c26ac8
 )
--- controller/server.go
+++ controller/server.go
@@ -4,56 +4,68 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/render"
 	cli "gopkg.in/urfave/cli.v2"
 )
 
 func server(c *cli.Context) error {
 	r := chi.NewRouter()
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
+	//r.Use(middleware.RequestID)
+	//r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Use(middleware.Timeout(60 * time.Second))
+	r.Use(middleware.Timeout(5 * time.Second))
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte(`
-<body bgcolor=black>
-  <audio autoload="true" autoplay="true" controls="true" id="player-background">
-    <source src="http://new.radio.lasuitedumonde.com:8000/radio-imaginee-160.mp3" type="audio/mpeg" />
-    <source src="http://new.radio.lasuitedumonde.com:8000/radio-imaginee-128.aac" type="audio/aac" />
-    <source src="http://new.radio.lasuitedumonde.com:8000/radio-imaginee-192.mp3" type="audio/mpeg" />
-    <source src="http://new.radio.lasuitedumonde.com:8000/radio-imaginee.ogg" type="audio/ogg" />
-    <source src="http://new.radio.lasuitedumonde.com:8000/radio-imaginee-128.mp3" type="audio/mpeg" />
-    <source src="http://new.radio.lasuitedumonde.com:8000/radio-imaginee-160.aac" type="audio/aac" />
-    <source src="http://new.radio.lasuitedumonde.com:8000/radio-imaginee-192.aac" type="audio/aac" />
-    <source src="http://new.radio.lasuitedumonde.com:8000/radio-imaginee-64.mp3" type="audio/mpeg" />
-  </audio>
-</body>
-`))
+	r.Route("/api", func(r chi.Router) {
+		r.Use(render.SetContentType(render.ContentTypeJSON))
+		r.Get("/info", func(w http.ResponseWriter, r *http.Request) {
+			info, err := getInfo(c)
+			out, err := json.MarshalIndent(info, "", "  ")
+			if err != nil {
+				// FIXME: display error
+				return
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(out)
+		})
+		r.Get("/skip", func(w http.ResponseWriter, r *http.Request) {
+			msg, err := skip(c)
+			if err != nil {
+				// FIXME: display error
+				return
+			}
+			w.Write([]byte(msg))
+		})
 	})
-	r.Get("/info", func(w http.ResponseWriter, r *http.Request) {
-		info, err := getInfo(c)
-		out, err := json.MarshalIndent(info, "", "  ")
-		if err != nil {
-			// FIXME: display error
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(out)
-	})
-	r.Get("/skip", func(w http.ResponseWriter, r *http.Request) {
-		msg, err := skip(c)
-		if err != nil {
-			// FIXME: display error
-			return
-		}
-		w.Write([]byte(msg))
-	})
+	workDir, _ := os.Getwd()
+	filesDir := filepath.Join(workDir, "www")
+	FileServer(r, "/", http.Dir(filesDir))
+
 	fmt.Printf("Starting server on %q...\n", c.String("bind"))
 	return http.ListenAndServe(c.String("bind"), r)
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	}))
 }
